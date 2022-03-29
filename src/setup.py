840a023ca3daf8a61498eb74dc1504d249ea30f8
Alicia Vázquez Ramos <aliciavr@iaa.es>
2022-03-29 13:42:19 +0200

    Setup version update

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ import setuptools as st
 
 st.setup(
     name = "thomsonpy",
-    version = "0.1",
+    version = "1.0-alpha",
     description = "Thomson Scattering tools and computations about the K - Corona",
     author = "",
     author_email = "",
