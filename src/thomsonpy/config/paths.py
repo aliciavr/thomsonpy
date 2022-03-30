2db8d40923a817492ef48835873a113aa7f973d7
Alicia Vázquez Ramos <aliciavr@iaa.es>
2022-03-30 14:06:10 +0200

    Fixed name parameter

--- src/thomsonpy/config/paths.py
+++ src/thomsonpy/config/paths.py
@@ -1,6 +1,8 @@
 PROJECT_NAME = "first-quadrant"
+PREDSCI_FILENAME = "rho002.hdf"
+
 OCTREE_DATA_PATH = f"data/projects/{PROJECT_NAME}/octree_data_format/"
 OCTREES_PATH = f"data/projects/{PROJECT_NAME}/octrees/"
 MODELS_PATH = f"data/projects/{PROJECT_NAME}/models/"
 PREDSCI_DATA_PATH = "data/predictive_science/eclipse2021_mhd_final/corona/"
-NE_PREDSCI_DATA_PATH = "data/predictive_science/eclipse2021_mhd_final/corona/rho002.hdf"
+
