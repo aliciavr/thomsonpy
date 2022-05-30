041150555aa60139dc06a21d9b9f8b2ad6d6153e
Alicia Vázquez Ramos <aliciavr@iaa.es>
2022-05-30 10:43:56 +0200

    6x6Rsun computing update

--- src/thomsonpy/config/solar_imager_params.py
+++ src/thomsonpy/config/solar_imager_params.py
@@ -34,7 +34,7 @@ IMAGE_MAX = np.array([MAX_COORD, MAX_COORD])
 IMAGE_MIN = np.array([MIN_COORD, MIN_COORD])
 """ Minimum coordinates of the image in :math:`m`. """
 
-IMAGE_RESOLUTION = 50000000 #  (50000 km)
+IMAGE_RESOLUTION = 7600000 #  (7600 km)
 """ Image resolution in :math:`m`. """
 
 IMAGE_NUM_POINTS = int(np.rint((MAX_COORD - MIN_COORD) / IMAGE_RESOLUTION))
