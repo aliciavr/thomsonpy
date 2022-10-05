b40831867a0d4ea4f11a11739cff2325db26cb7e
Alicia Vázquez Ramos <aliciavr@iaa.es>
2022-10-05 13:47:52 +0200

    Paths updated for the new main

--- src/thomsonpy/config/paths.py
+++ src/thomsonpy/config/paths.py
@@ -4,21 +4,27 @@
         :synopsis: package relative paths for loading and storing models.
 .. moduleauthor:: Alicia Vázquez Ramos (SPG - IAA) <aliciavr@iaa.es>
 """
+PREFIX = "../../"
+RAW_MODELS_EXTENSION = "hdf"
+DATA_MODELS_EXTENSION = "model"
+DATA_STRUCTURES_EXTENSION = "dstruct"
+IMAGE_MODELS_EXTENSION = "imodel"
 
 PROJECT_NAME = "cmag"
 """ Path to the base folder of the project in execution. """
 
-PREDSCI_FILENAME = "rho002.hdf"
-""" Filename of the raw model. """
-
-PREDSCI_DATA_PATH = "data/predictive_science/eclipse2021_mhd_final/corona/"
+RAW_MODELS_PATH = f"{PREFIX}data/input/eclipse2021_mhd_final/corona/"
 """ Path to the base folder of the raw models provided by Predictive Science Inc. """
 
-OCTREE_DATA_PATH = f"data/projects/{PROJECT_NAME}/data_format/"
+RAW_FILENAME = [f"rho002.{RAW_MODELS_EXTENSION}", f"t002.{RAW_MODELS_EXTENSION}"]
+""" Filename of the raw model. """
+
+DATA_MODELS_PATH = f"{PREFIX}data/output/{PROJECT_NAME}/data_models/"
 """ Path to the formatted data. """
 
-OCTREES_PATH = f"data/projects/{PROJECT_NAME}/data_structures/"
+STRUCTURES_PATH = f"{PREFIX}data/output/{PROJECT_NAME}/structures/"
 """ Path to the folder containing the binary objects with the data structures. """
 
-MODELS_PATH = f"data/projects/{PROJECT_NAME}/models/"
+IMODELS_PATH = f"{PREFIX}data/output/{PROJECT_NAME}/imodels/"
 """ Path to the folder containing the final image models of the Thomson scattering computation. """
+
